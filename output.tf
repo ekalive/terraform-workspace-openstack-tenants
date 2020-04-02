9393a15e43f11c9326048ade2cb755d11f3e6a82
Taylor Owen <as0bu@users.noreply.github.com>
2020-04-02 09:55:09 -0600

    Merging development branch into master

---
+++ output.tf
@@ -0,0 +1,19 @@
+output "tenant_name" {
+  value = module.app_a.tenant_name
+}
+
+output "environment" {
+  value = module.app_a.environment
+}
+
+output "network_name" {
+  value = module.app_a.network_name
+}
+
+output "subnet_name" {
+  value = module.app_a.subnet_name
+}
+
+output "subnet_resource" {
+  value = module.app_a.subnet_resource
+}
